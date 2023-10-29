33def94572401a92c0317782b34e44a5751cd6e6
Sinan Demir <sinanmaraz4@gmail.com>
2023-10-29 23:45:16 +0300

    Question 4 is added

---
+++ src/Question_4/Question_4.java
@@ -0,0 +1,20 @@
+package Question_4;
+
+public class Question_4 {
+    public static int factorial(int num){
+        int first = 1;
+        for (int i=1;i<=num;i++)
+        {
+            first=first*i;
+        }
+        return first;
+    }
+    public static void main(String[] Sinan){
+
+
+
+
+
+
+    }
+}
